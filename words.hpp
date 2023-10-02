8179b31c5d4ce62da7a017724e37412438e21ba2
Lazar Djurovic <lazardjurovic59@gmail.com>
2023-10-02 17:59:36 +0200

    adding more words

--- words.hpp
+++ words.hpp
@@ -8,7 +8,33 @@
 using namespace std;
 
 vector<string> base_words = {"!",".", "+", "-", "*","/", "*/", "0<", "0=", "0>", "1+", "1-", "2+", "2-",
-"<", "=", ">", "?DUP", "DUP","ABS","AND","XOR","OR", "MAX", "MIN", "MOD", "OVER"};
+"<", "=", ">", "?DUP", "DUP","ABS","AND","XOR","OR", "MAX", "MIN", "MOD", "OVER", "SWAP", "DROP", "MOVE",
+"NEGATE","PICK", "@", "DEPTH", "FILL"};
+
+int findNthFromTop(std::stack<int> st, int n) { // AI generated
+    if ( n > st.size() || n<1) {
+        std::cerr << "Invalid value of n or stack size." << std::endl;
+        return -1; // Return an error value
+    }
+
+    std::vector<int> tempVec;
+
+    // Pop elements from the stack and store them in a temporary vector
+    while (!st.empty()) {
+        tempVec.push_back(st.top());
+        st.pop();
+    }
+
+    // Get the n-th element from the top
+    int nthElement = tempVec[n];
+
+    // Restore the original stack
+    for (int i = tempVec.size() - 1; i >= 0; i--) {
+        st.push(tempVec[i]);
+    }
+
+    return nthElement;
+}
 
 int find_word(string w){
     for(int i=0; i<base_words.size();i++){
@@ -23,7 +49,7 @@ void execute_word(string w, stack<int> &s,int* mem){
 
     if(w.compare(".")==0){
         if(s.size() != 0){
-            printf("%d\n", s.top());
+            printf("%d", s.top());
             s.pop();
         }else{
             printf("Data stack is empty!\n");
@@ -209,6 +235,55 @@ void execute_word(string w, stack<int> &s,int* mem){
         int b = s.top();
         s.push(a);
         s.push(b);
+    }
+    else if(w.compare("SWAP")==0){
+       int a = s.top();
+       s.pop();
+       int b = s.top();
+       s.pop();
+       s.push(a);
+       s.push(b);
+    }else if(w.compare("DROP")==0){
+        s.pop();
+    }else if(w.compare("MOVE")==0){
+        int addr1 = s.top();
+        s.pop();
+        int addr2 = s.top();
+        s.pop();
+        int n = s.top();
+        s.pop();
+
+        for(int i = 0; i<n;i++){
+            mem[addr2+i] = mem[addr1+i];
+        }
+
+    }else if(w.compare("NEGATE")==0){
+        int n = s.top();
+        s.pop();
+        s.push(-n);
+
+    }else if(w.compare("PICK")==0){
+        int n = s.top();
+        s.pop();
+        s.push(findNthFromTop(s,n));
+    }else if(w.compare("@")==0){
+        int addr = s.top();
+        s.pop();
+        s.push(mem[addr]);
+    }else if(w.compare("DEPTH")==0){
+        s.push(s.size());
+    }else if(w.compare("FILL")==0){
+        int byte = s.top();
+        s.pop();
+        int n = s.top();
+        s.pop();
+        int addr = s.top();
+        s.pop();
+        if(n>0){
+            for(int i=0;i<n;i++){
+                mem[addr+i] = byte;
+            }
+        }
     }
 
 }
